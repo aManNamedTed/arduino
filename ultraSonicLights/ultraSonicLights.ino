75636a52e5a1fddd726f70f3e4318991c5f5d802
David Amante <davidamante17@yahoo.com>
2018-08-17 13:30:02 -0700

    ultrasonic light code upload

---
+++ ultraSonicLights/ultraSonicLights.ino
@@ -0,0 +1,42 @@
+const int trigPin = 2;
+const int echoPin = 3;
+int ledPinArr[5];
+int startPin = 9;
+int numLeds = 5;
+int triggerDistance = 25;
+
+long duration;
+int distance;
+
+void setup() {
+  for(int i = 0; i < numLeds; i++) { ledPinArr[i] = i + startPin;   }
+  for(int i = 0; i < numLeds; i++) { pinMode(ledPinArr[i], OUTPUT); }
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+//  Serial.print("Distance: ");
+//  Serial.println(distance);
+
+  if(distance < triggerDistance)
+  {
+    for(int i = 0; i < numLeds; i++) { digitalWrite(ledPinArr[i], HIGH); }                    
+  }
+  else
+  {
+    for(int i = 0; i < numLeds; i++) { digitalWrite(ledPinArr[i], LOW); }                    
+  }
+}
