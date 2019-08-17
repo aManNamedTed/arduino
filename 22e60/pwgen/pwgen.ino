bcb7a20b22dfa29e248a181972c55d932391600e
aManNamedTed <davidamante17@yahoo.com>
2019-08-17 11:25:35 -0700

    analog functionality complete

--- 22e60/pwgen/pwgen.ino
+++ 22e60/pwgen/pwgen.ino
@@ -1,13 +1,25 @@
+#include <dht.h>
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int leds[] = {10, 9, 8, 7};
+dht DHT;
+
+int echoPin = A1;
+int trigPin = A0;
 int buttons[] = {A2, A3, A4, A5};
 int buttonStates[] = {0, 0, 0, 0};
+int leds[] = {10, 9, 8, 7};
+int ledStates[] = {0, 0, 0, 0};
+int humidityPin = 13;
+int pwGenPin = 6;
+int pwGenPinState = 0;
 int n = (sizeof(buttonStates) / sizeof(buttonStates[0]));
+
 void setup() {
-  // put your setup code here, to run once:
-  //lcd.begin(16, 2);
+  lcd.begin(16, 2);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
   for(int i = 0; i < n; i++) {
     pinMode(leds[i], OUTPUT);
     pinMode(buttons[i], INPUT);
@@ -15,22 +27,35 @@ void setup() {
 }
 
 void loop() {
+  int check = DHT.read11(humidityPin);
+  Serial.println(DHT.temperature);
+  Serial.println(DHT.humidity);
+  digitalWrite(trigPin, LOW);
+  delay(10);
+  digitalWrite(trigPin, HIGH);
+  delay(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
   for(int i = 0; i < n; i++) {
     buttonStates[i] = digitalRead(buttons[i]);
 
-    if(buttonStates[i] == HIGH or buttonStates[i]) {
-      digitalWrite(leds[i], HIGH);
-      buttonStates[i] = 1;
-    } else {
-      digitalWrite(leds[i], LOW);
-      buttonStates[i] = 0;
+    if(buttonStates[i] == HIGH) {
+      if(ledStates[i] == false) {
+        ledStates[i] = true;
+        digitalWrite(leds[i], HIGH);
+      } else {
+        ledStates[i] = false;
+        digitalWrite(leds[i], LOW);  
+      }
+      delay(111);
     }
   }
-  
-  //lcd.print("");
-  /*
-  delay(500);
-  lcd.clear();
-  delay(500);
-  */
+  pwGenPinState = digitalRead(pwGenPin);
+  if(pwGenPinState == HIGH) {
+    lcd.print("abz]q'w/e.>;<][dSe5%47&83#9$)*&");  
+  }
 }
